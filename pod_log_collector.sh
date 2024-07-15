e15618488cdd42fbaf3d1afd55d764dba3a05237
Andy <62133915+cooloo9871@users.noreply.github.com>
2024-07-15 15:22:38 +0800

    Create pod_log_collector.sh

---
+++ pod_log_collector.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+help() {
+  cat <<EOF
+Usage: pod_log_collect.sh [NAMESPACE]
+
+EOF
+  exit
+}
+
+if [[ -z "$1" ]]; then
+  help && exit 1
+fi
+if [[ ! -d "~/log" ]]; then
+  mkdir ~/log
+fi
+
+NS_LIST="$1"
+
+for ns in ${NS_LIST};
+do
+    POD_LIST=$( kubectl -n $ns get pod --no-headers | awk '{print $1}' )
+    for pod in $POD_LIST;
+    do
+        # 提示 previous terminated container 可以忽略
+        kubectl -n $ns logs $pod --all-containers 2>&1 > ~/log/$ns-$pod.log
+        kubectl -n $ns logs $pod --all-containers -p 2>&1 > ~/log/$ns-$pod-p.log
+    done
+done
